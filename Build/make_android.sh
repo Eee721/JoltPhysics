cc75fd9aad3c85ad2f4c8056226fd9e8213895b2
Eee721 <454180070@qq.com>
2023-03-08 02:57:45 +0800

    update
    update

--- Build/make_android.sh
+++ Build/make_android.sh
@@ -5,7 +5,7 @@ elif [ -n "$ANDROID_NDK_HOME" ]; then
 elif [ -n "$ANDROID_NDK_HOME" ]; then
     export NDK=${ANDROID_NDK_HOME}
 else
-    export NDK=~/android-ndk-r15c
+    export NDK=/Users/qiyongyi/Documents/ndk/AndroidNDK9519653.app/Contents/NDK
 fi
 
 if [ ! -d "$NDK" ]; then
@@ -21,9 +21,9 @@ function build() {
     cmake -H. -B${BUILD_PATH} -DANDROID_ABI=${ABI} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${API} -DANDROID_TOOLCHAIN=clang -DANDROID_TOOLCHAIN_NAME=${TOOLCHAIN_ANME}
     cmake --build ${BUILD_PATH} --config Release
     mkdir -p plugin_jolt/Plugins/Android/libs/${ABI}/
-    cp ${BUILD_PATH}/libjolt.so plugin_jolt/Plugins/Android/libs/${ABI}/libjolt.so
+    cp ${BUILD_PATH}/libjoltc.so plugin_jolt/Plugins/Android/libs/${ABI}/libjoltc.so
 }
 
-build android-16 armeabi-v7a arm-linux-androideabi-4.9
+# build android-19 armeabi-v7a arm-linux-androideabi-4.9
 build android-16 arm64-v8a  arm-linux-androideabi-clang
-build android-16 x86 x86-4.9
+# build android-16 x86 x86-4.9
