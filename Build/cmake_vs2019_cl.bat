c783261ba4911eba868d2e97f86486d442943b40
Eee721 <454180070@qq.com>
2023-03-07 22:49:15 +0800

    update

--- Build/cmake_vs2019_cl.bat
+++ Build/cmake_vs2019_cl.bat
@@ -1,3 +1,7 @@
 @echo off
 cmake -S . -B VS2019_CL -G "Visual Studio 16 2019" -A x64
 echo Open VS2019_CL\JoltPhysics.sln to build the project.
+
+rem cmake --build VS2019_CL --config Distribution
+
+cmake --build .\VS2019_CL --config Distribution
